043ffcdbfa0f39dbd17c0fdad5839761f7794275
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2023-07-03 18:01:26 -0600

    feat: Modified 'info' command
    Instead of dm to the user and sent all of the commands, now show only the name of the commands, if the users whats to know more exist another command 'info <command>' which return all info of that command

--- commands/info/comandos.js
+++ commands/info/comandos.js
@@ -1,43 +1,45 @@
 const { EmbedBuilder } = require('discord.js');
+const commands = require('./commands.json');    // get all commands
+
+const categories = {};
+
+for (const command of commands) {   // get all commands divided by categories
+    const category = command.category;
+    if (!categories.hasOwnProperty(category)) // if it doesn't exist create it
+        categories[category] = [];
+    categories[category].push(command.name);    // add command to category
+}
 
 module.exports = {
-    name: "comandos",
-    aliases: ["info", "información", "información", "help", "ayuda", "commands"],
-    desc: "¿Quieres conocer los comandos del bot para los INCOgibles?",
-    run: async (client, message, args) => {
-        const Embed = new EmbedBuilder()
-            .setTitle(`Comandos del INCO-bot 🤑`)
-            .setDescription('INCO-bot mamalon')
-            .addFields(
-                // { name: "!cornejo", value: "Una dedicatoria cuando lo recuerdes <:REEeee:901173179106623538>"}, 
-                // { name: "!horacio", value: "<:linux:821495324149415946> "},
-                // { name: "!ctmpro", value: "Cuando no sea PRO <:pront:888504671470252083> "},
-                // { name: "!pro", value: "¿Cansado de contar por ti mismo las PRO menciones del PRO? Deja que el bot lo haga por tí ✔"},
-                { name: "!pito", value: "Deja que el bot adivine cuanto te mide el pito 🥵"},
-                { name: "!pareja", value: "Descubre cuanta probabilidad tienes de ser pareja con un usuario al azar del servidor 👀"},
-                { name: "!pareja <usuario>", value: "Descubre cuanta probabilidad tienes de ser pareja con el usuario ingresado 😈"},
-                { name: "!volado", value: "Aguila o Sello, no hay más que agregar"},
-                { name: "!equipos <participantes separados por espacio> <Cantidad de integrantes>", value: "¿Cansado de no saber como formar equipos para la escuela? ¡Este comando te ayuda en eso!"},
-                { name: "!ctm <usuario>", value: "Recuerdale al usuario su madresita"},
-                { name: "!morraschidas", value: "( ͡° ͜ʖ ͡°)"},
-                { name: "!monaschinas", value: "By: Alex"},
-                { name: "!server", value: "Wacha la información de los servers actuales"},
-                { name: "!clima <lugar>", value: "Obten el clima del lugar que quieras"},
-                { name: "!encuesta <pregunta>.<opcion1>.<opcion2>...", value: "Crea encuesta de manera fácil, solo separa tanto la pregunta como las opciones con un punto <.>"},
-                { name: "**Sección de Música**", value: '*Necesitas estar en un canal de voz*' },
-                { name: "!play <canción>", value: "Reproducir una canción 🎵" },
-                { name: "!queue", value: "Ve la cola de reproducción (no lo uses si hay demasiadas canciones, morirá el bot)" },
-                { name: "!shuffle", value: "Mezcla la cola de reproducción si no estas conforme con el orden" },
-                { name: "!nowplaying", value: "Obtén el nombre de la canción que se este escuchando" },
-                { name: "!skip", value: "Quita alv la canción" },
-                { name: "!volume <numero>", value: "Súbele o bájale, tu decides" },
-                { name: "!repeat <no | song | queue>", value: "Modo de repetir, por default es NO" },
-                { name: "!clear", value: "Limpia la cola de reproducción (unicamente se quedará la que esté sonando en el momento)" },
-                { name: "!leave", value: "No dejes al bot solo, sacalo del canal de voz" }
-            )
-            .setThumbnail('https://i.imgur.com/E4w0mce.jpg')
-            .setColor('#0099ff');
-        client.users.cache.get(message.author.id).send( { embeds: [Embed] } );
-        message.channel.send(`${message.author} checa tus mensajes 👀`);
+    name:   "comandos",
+    aliases:    ["info", "información", "información", "help", "ayuda", "commands"],
+    desc:   "¿Quieres conocer los comandos del bot para los INCOgibles?",
+    run:    async (client, message, args) => {
+        if(args.length === 0){
+            const Embed = new EmbedBuilder()
+                .setTitle(`Comandos del INCO-bot 🤑`)
+                .setDescription('INCO-bot mamalon');
+                
+            for( const category in categories)  // Add all commands divided by categories
+                Embed.addFields( { name: `Comandos ${category}`, value: categories[category].join(' ▪ ')} );
+                
+            Embed.setThumbnail('https://i.imgur.com/E4w0mce.jpg')
+                .setFooter( { text: "Usa '!info <comando>' para más información del comando"} )
+                .setColor('#0099ff');
+
+            return message.channel.send( { embeds:   [Embed] });
+        }
+        args.forEach( cmd => {
+            cmd = cmd.toLowerCase();
+            
+            let comando = Object.entries(commandsGeneral).concat(Object.entries(commandsMusic)).filter(([k]) => k.includes(cmd) );
+            comando = comando.length ? comando.flat() : ['✖','Comando no encontrado'];   // command.length = 0 means NOT FOUND
+            
+            const Embed = new EmbedBuilder()
+                .setTitle(`Comando ${comando[0]}`)
+                .setDescription( comando[1] )
+                .setColor("#A5CDE8");
+            message.channel.send( { embeds:   [Embed] });
+        })
     }
 };
