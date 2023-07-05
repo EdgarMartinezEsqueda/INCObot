217ec98efe53ea9a748b423cd08830788ca547a5
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2023-07-04 18:04:21 -0600

    refactor: Updated 'ctm' command
    The user can now use it by tagging many users instead of just one.

--- commands/mentadas/ctm.js
+++ commands/mentadas/ctm.js
@@ -3,10 +3,12 @@ module.exports = {
     desc: "Mentale su madre a alguien",
     run: async (client, message, args) => {
         if( message.mentions.users.first() ){
-            message.channel.send({ 
-                content: `Chingas a tu madre ${ message.mentions.users.first() }`,
-                tts: true 
-            });  
+            message.mentions.users.map( user => 
+                message.channel.send({ 
+                    content: `Chingas a tu madre <@${ user.id }>`,
+                    tts: true 
+                }) 
+            );  
         }
         else
             return message.channel.send(`Tienes que mencionar a alguien, ${message.author} pendejo`, { tts: true });
