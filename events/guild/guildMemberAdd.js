7650f589127908d64f9a4fb47cd5083d79036acb
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2023-09-08 02:24:38 -0600

    fix: :bug: Updated guild events
    Now works xdd

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,4 +1,4 @@
-module.exports = async (client,  userStates, message) => {
+module.exports = async (client,  userStates, member) => {
     const images = [
         "https://scontent.fgdl14-1.fna.fbcdn.net/v/t1.6435-9/40484279_466330137213593_2540222282249273344_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=iYLmGWWInQ8AX9mOyLT&_nc_ht=scontent.fgdl14-1.fna&oh=00_AfAbnpruRErVc5XYFVoOtP2irp95jPNqNH3AnSDtVibRhQ&oe=6521A6A6",
         "https://i.pinimg.com/originals/fb/e2/f3/fbe2f31edcf006afa50a3c3b950553ad.jpg",
@@ -6,5 +6,11 @@ module.exports = async (client,  userStates, message) => {
         "https://i.pinimg.com/564x/71/71/c0/7171c0817a43567a9dfdf7135ac8abea.jpg"
     ]
     const random = Math.floor( Math.random() * images.length );
-	message.reply( images[random] );
+    const channel = member.guild.channels.cache.find(ch => ch.name === "puros-pretextos" || ch.name === "general");
+    await channel.fetch();
+
+    channel.send( {
+        content: `Bienvenido ${member.user}!`,
+        files: [images[random]]
+    } );
 };
--- events/guild/guildMemberRemove.js
+++ events/guild/guildMemberRemove.js
@@ -1,9 +1,15 @@
-module.exports = async (client,  userStates, message) => {
+module.exports = async (client,  userStates, member) => {
     const images = [
         "https://plantillasdememes.com/img/plantillas/pluto-muerto-y-mickey-mouse-llorando01576572909.jpg",
         "https://i.postimg.cc/nzpLnhgL/317648644-207491841668668-7552572720474324509-n.jpg",
         "https://i.postimg.cc/HsHF375n/276111087-1892508040950442-5144807186762423371-n.jpg"
-    ]
+    ];
     const random = Math.floor( Math.random() * images.length );
-	message.reply( images[random] );
+    const channel = member.guild.channels.cache.find(ch => ch.name === "puros-pretextos" || ch.name === "general");
+    await channel.fetch();
+
+    channel.send( {
+        content: `Le dieron democracia a ${member.user} <:democracia:783839090737938482>`,
+        files: [images[random]]
+    } );
 };
