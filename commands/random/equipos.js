2a14699e56da2473f51d5f928324bf80f3c1107c
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2023-09-18 00:49:03 -0600

    fix: :bug: Updated 'equipos' command
    Bugs fixed, returns a Embed Message instead of a lot of plain text messages

--- commands/random/equipos.js
+++ commands/random/equipos.js
@@ -1,30 +1,37 @@
+const { EmbedBuilder, Formatters, Embed } = require('discord.js');
+
 module.exports = {
     name: "equipos",
     desc: "¿Cansado de no saber como formar equipos para la escuela? ¡Este comando te ayuda en eso!",
     run: async (client, message, args) => {
         try{
-            let partes = message.content.split(" ");  //Se le hace split de los espacios
-            let num = partes.pop(); // se quita el ultimo, el cual es el numero
-            partes.shift(); //se quita la primera posicion es decir el comando (!equipos)
+            let num = args.pop(); // The last number HAS TO BE A NUMBER
+            //Check if the last argument is a number
+            if( isNaN(num) )   return message.reply(`Si no sabes que vrga haces para eso está '!info <comando>'`);	
+
             let equipos = [];
-            let loop = partes.length / num;
-            for(let i = 0;  i < loop; i++){
+            let loop = args.length / num;
+
+            for(let i = 0;  i < loop; i++){ // Loop for create the teams
                 let aux = [];
                 for(let j = 0; j < num; j++){
-                    let pos = Math.floor( Math.random() * (partes.length - 0) );
-                    aux.push(partes[pos]);
-                    partes.splice(pos,1);
+                    let pos = Math.floor( Math.random() * (args.length - 0) );
+                    aux.push(args[pos]);
+                    args.splice(pos,1);
                 }
                 equipos.push(aux);
             }
-            message.channel.send("Equipos creados");
-            for(let i = 0; i < equipos.length; i ++){
-                message.channel.send(`**Equipo #${i+1}**`);
-                message.channel.send(`${equipos[i]}`);
-            }
+            // Add the teams to the embed message
+            const Embed = new EmbedBuilder()
+                .setTitle("Equipos Creados 👷‍♂️");
+            for(let i = 0; i < equipos.length; i ++)
+                Embed.addFields({ name: `**Equipo #${i+1}**`, value: equipos[i].join(", ") });
+            Embed.setColor("#B7E60A");
+
+            message.channel.send({ embeds: [Embed] });
         }
         catch (e){
-            message.channel.send(`${message.author}`,{files:["images/?.jpg"]});
+            message.channel.send(`Sepa la vrga que pasó pero peto esta mmda ${e}`);
         }
     }
 };
