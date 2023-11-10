433bd341c40e0b7683d1ee9e79348db6e9f74750
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2023-11-09 19:15:49 -0600

    fix: GuildMemberAdd images reponses updated

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,6 +1,6 @@
 module.exports = async (client,  userStates, member) => {
     const images = [
-        "https://scontent.fgdl14-1.fna.fbcdn.net/v/t1.6435-9/40484279_466330137213593_2540222282249273344_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=iYLmGWWInQ8AX9mOyLT&_nc_ht=scontent.fgdl14-1.fna&oh=00_AfAbnpruRErVc5XYFVoOtP2irp95jPNqNH3AnSDtVibRhQ&oe=6521A6A6",
+        "https://pm1.aminoapps.com/6623/950f8ae66208a0afcab7bfe7374994549e9378d5_00.jpg",
         "https://i.pinimg.com/originals/fb/e2/f3/fbe2f31edcf006afa50a3c3b950553ad.jpg",
         "https://images3.memedroid.com/images/UPLOADED355/607743e627991.jpeg",
         "https://i.pinimg.com/564x/71/71/c0/7171c0817a43567a9dfdf7135ac8abea.jpg"
